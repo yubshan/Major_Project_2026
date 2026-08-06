6e87592e82df607edb44e3c608d5cc020054f126
wjxnl <0aug.anusha@gmail.com>
2026-08-06 10:55:54 +0545

    FIX: Unnecessary comments taken out.

--- shared/blackboard.py
+++ shared/blackboard.py
@@ -1,13 +1,6 @@
 # Requirement / Purpose: Houses the custom, thread-safe central dictionary class that holds the global state of the robot. 
 # It uses threading locks (threading.Lock()) to prevent data corruption.
 
-# Why it is needed: Your modules run concurrently on separate threads. 
-# If Yubshan's WiFi module tries to write a human position at the exact microsecond 
-# Teammate B's decision tree tries to read it, Python will throw a fatal runtime error or corrupt the data. 
-# This file safely gates access so only one thread can touch the data at a time.
-
-
-
 import threading  # provides a Lock through the threading module.
 
 class Blackboard:
